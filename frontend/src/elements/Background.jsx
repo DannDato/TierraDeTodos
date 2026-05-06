e625f813ce8536801a862dbda493a531ec1584bb
danndato <danieltova97@gmail.com>
2026-05-06 15:51:43 -0600

    commit inicial para corregir problemas de rendimiento causados por blurs excesivos

--- frontend/src/elements/Background.jsx
+++ frontend/src/elements/Background.jsx
@@ -18,7 +18,7 @@ function Background({
       <div
         className="fixed top-0 left-0 w-full h-full bg-cover bg-center scale-110"
         style={{
-          backgroundImage: `url(${backgroundImage})`,
+          // backgroundImage: `url(${backgroundImage})`,
           filter: `blur(${blur})`,
           zIndex: 0,
         }}
